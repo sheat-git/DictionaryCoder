6012241e23a64c4fb8676bb24315b4977dabd626
sheat <dev.sheat@gmail.com>
2023-01-16 01:04:18 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DictionaryCoder
+
+A description of this package.
---
+++ Sources/DictionaryCoder/DictionaryCoder.swift
@@ -0,0 +1,6 @@
+public struct DictionaryCoder {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
